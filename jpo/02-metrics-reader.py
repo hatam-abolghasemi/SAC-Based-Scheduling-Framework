8c1939d98f059d4c728896d69457cb657f78b15d
hatam-abolghasemi <hatamabolghasemi@gmail.com>
2024-10-04 19:52:37 +0330

    just a quick backup

--- jpo/02-metrics-reader.py
+++ jpo/02-metrics-reader.py
@@ -3,16 +3,17 @@ import time
 from flask import Flask, Response
 
 PROMETHEUS_URL = 'http://127.0.0.1:9090'
-CPU_UTILIZATION_QUERY = 'avg(100 - (avg by(instance) (irate(node_cpu_seconds_total{job="local_node_exporter", mode="idle"}[5m])) * 100))'
-MEMORY_UTILIZATION_QUERY = 'avg((1 - (node_memory_MemAvailable_bytes{job="local_node_exporter"} / node_memory_MemTotal_bytes{job="local_node_exporter"})) * 100)'
+NODE_CPU_UTILIZATION_QUERY = 'avg(100 - (avg by(instance) (irate(node_cpu_seconds_total{job="local_node_exporter", mode="idle"}[5m])) * 100))'
+NODE_MEMORY_UTILIZATION_QUERY = 'avg((1 - (node_memory_MemAvailable_bytes{job="local_node_exporter"} / node_memory_MemTotal_bytes{job="local_node_exporter"})) * 100)'
+CONTAINER_CPU_UTILIZATION_QUERY = 'sum(rate(container_cpu_usage_seconds_total{image!=""}[1m])) by (name, image)'
 
 app = Flask(__name__)
 
 metrics_data = ''
 
-def fetch_cpu_utilization():
+def fetch_node_cpu_utilization():
     global metrics_data
-    response = requests.get(f'{PROMETHEUS_URL}/api/v1/query', params={'query': CPU_UTILIZATION_QUERY})
+    response = requests.get(f'{PROMETHEUS_URL}/api/v1/query', params={'query': NODE_CPU_UTILIZATION_QUERY})
     if response.status_code == 200:
         data = response.json()
         if data['status'] == 'success':
@@ -20,11 +21,11 @@ def fetch_cpu_utilization():
             if results:
                 value = float(results[0]['value'][1])
                 unix_time = int(time.time())
-                metrics_data = f'{unix_time} cpu_utilization {value:.2f}%\n'
+                metrics_data += f'{unix_time} node_cpu_utilization {value:.2f}%\n'
 
-def fetch_memory_utilization():
+def fetch_node_memory_utilization():
     global metrics_data
-    response = requests.get(f'{PROMETHEUS_URL}/api/v1/query', params={'query': MEMORY_UTILIZATION_QUERY})
+    response = requests.get(f'{PROMETHEUS_URL}/api/v1/query', params={'query': NODE_MEMORY_UTILIZATION_QUERY})
     if response.status_code == 200:
         data = response.json()
         if data['status'] == 'success':
@@ -32,7 +33,21 @@ def fetch_memory_utilization():
             if results:
                 value = float(results[0]['value'][1])
                 unix_time = int(time.time())
-                metrics_data += f'{unix_time} memory_utilization {value:.2f}%\n'
+                metrics_data += f'{unix_time} node_memory_utilization {value:.2f}%\n'
+
+def fetch_container_cpu_utilization():
+    global metrics_data
+    response = requests.get(f'{PROMETHEUS_URL}/api/v1/query', params={'query': CONTAINER_CPU_UTILIZATION_QUERY})
+    if response.status_code == 200:
+        data = response.json()
+        if data['status'] == 'success':
+            results = data['data']['result']
+            unix_time = int(time.time())
+            for result in results:
+                name = result['metric'].get('name', 'unknown_container')
+                image_name = result['metric'].get('image', 'unknown_image')
+                value = float(result['value'][1])
+                metrics_data += f'{unix_time} container_cpu_utilization(image:{image_name},container:{name}) {value:.2f}%\n'
 
 @app.route('/metrics', methods=['GET'])
 def get_metrics():
@@ -42,13 +57,16 @@ if __name__ == "__main__":
     # Start a background thread to fetch metrics periodically
     def fetch_metrics():
         while True:
-            fetch_cpu_utilization()
-            fetch_memory_utilization()
+            global metrics_data
+            metrics_data = ''  # Reset metrics data before each fetch cycle
+            fetch_node_cpu_utilization()
+            fetch_node_memory_utilization()
+            fetch_container_cpu_utilization()
             time.sleep(3)
 
     import threading
     threading.Thread(target=fetch_metrics, daemon=True).start()
 
     # Start the Flask application
-    app.run(host='127.0.0.1', port=4223)
+    app.run(host='0.0.0.0', port=4223)
 

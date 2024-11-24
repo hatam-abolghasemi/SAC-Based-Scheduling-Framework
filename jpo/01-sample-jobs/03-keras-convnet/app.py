c7419b4070b8109c5b3ed5a3db5f005047053d23
AylinNaebzadeh <a_naeb@comp.iust.ac.ir>
2024-11-24 06:39:31 -0500

    [fix] : bug in 03-keras-convnet

--- jpo/01-sample-jobs/03-keras-convnet/app.py
+++ jpo/01-sample-jobs/03-keras-convnet/app.py
@@ -10,7 +10,6 @@ Accelerator: GPU
 """
 ## Setup
 """
-
 import numpy as np
 import keras
 from keras import layers
@@ -21,7 +20,7 @@ from keras import layers
 
 # Model / data parameters
 num_classes = 10
-input_shape = (28, 28, 1)
+input_shape = (32, 32, 3)
 
 # Load the data and split it between train and test sets
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -29,15 +28,12 @@ input_shape = (28, 28, 1)
 # Scale images to the [0, 1] range
 x_train = x_train.astype("float32") / 255
 x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
+
 print("x_train shape:", x_train.shape)
 print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
-
-# convert class vectors to binary class matrices
+# Convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 

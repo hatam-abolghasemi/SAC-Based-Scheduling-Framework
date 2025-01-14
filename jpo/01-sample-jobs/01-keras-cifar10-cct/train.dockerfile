3c18e259458f6d109d5e6e2f99dc999cf366ca36
Hatam Abolghasemi <hatamabolghasemi@gmail.com>
2025-01-14 15:49:38 +0330

    add 4 jobs to metric-fetcher

--- jpo/01-sample-jobs/01-keras-cifar10-cct/train.dockerfile
+++ jpo/01-sample-jobs/01-keras-cifar10-cct/train.dockerfile
@@ -8,5 +8,12 @@ COPY . .
 # Link dataset from dataset container
 COPY --from=cifar10:1.0.3 /app/data /data
 
+LABEL framework="keras"
+LABEL dataset="cifar10"
+LABEL model="cct"
+LABEL batch_size=128
+LABEL learning_rate=0.001
+LABEL num_epochs=15
+
 ENTRYPOINT ["python", "train.py"]
 
--- jpo/03-metric-fetcher/app.py
+++ jpo/03-metric-fetcher/app.py
@@ -2,8 +2,7 @@ import requests
 import time
 from flask import Flask, Response
 
-# PROMETHEUS_URL = 'http://0.0.0.0:9090'
-PROMETHEUS_URL = 'http://192.168.80.132:9090'
+PROMETHEUS_URL = 'http://172.16.104.253:9090'
 NODE_CPU_UTILIZATION_QUERY = 'avg(100 - (avg by(instance) (irate(node_cpu_seconds_total{job="node-exporter", mode="idle"}[5m])) * 100))'
 NODE_MEMORY_UTILIZATION_QUERY = 'avg((1 - (node_memory_MemAvailable_bytes{job="node-exporter"} / node_memory_MemTotal_bytes{job="node-exporter"})) * 100)'
 CONTAINER_CPU_UTILIZATION_QUERY = 'sum(rate(container_cpu_usage_seconds_total{job="cadvisor",image!=""}[1m])) by (name, image, container_label_dataset, container_label_framework, container_label_model, container_label_batch_size, container_label_learning_rate, container_label_num_epochs)'
@@ -45,18 +44,25 @@ def fetch_container_cpu_utilization():
             results = data['data']['result']
             unix_time = int(time.time())
             for result in results:
-                # Check if the necessary container labels exist
-                if all(label in result['metric'] for label in ['container_label_dataset', 'container_label_framework', 'container_label_model', 'container_label_batch_size', 'container_label_learning_rate', 'container_label_num_epochs']):
-                    name = result['metric'].get('name', 'unknown_container')
-                    image_name = result['metric'].get('image', 'unknown_image')
-                    container_label_dataset = result['metric'].get('container_label_dataset', 'unknown_dataset')
-                    container_label_framework = result['metric'].get('container_label_framework', 'unknown_framework')
-                    container_label_model = result['metric'].get('container_label_model', 'unknown_model')
-                    container_label_batch_size = result['metric'].get('container_label_batch_size', 'unknown_batch_size')
-                    container_label_learning_rate = result['metric'].get('container_label_learning_rate', 'unknown_learning_rate')
-                    container_label_num_epochs = result['metric'].get('container_label_num_epochs', 'unknown_num_epochs')
-                    value = float(result['value'][1])
-                    metrics_data += f'{unix_time} container_cpu_utilization(image:{image_name},container:{name},dataset:{container_label_dataset},framework:{container_label_framework},model:{container_label_model},batch_size:{container_label_batch_size},learning_rate:{container_label_learning_rate},num_epochs:{container_label_num_epochs}) {value:.2f}%\n'
+                # Extract labels, defaulting to 'unknown_<label_name>' if not present
+                labels = {
+                    'name': result['metric'].get('name', 'unknown_container'),
+                    'image': result['metric'].get('image', 'unknown_image'),
+                    'dataset': result['metric'].get('container_label_dataset', 'unknown_dataset'),
+                    'framework': result['metric'].get('container_label_framework', 'unknown_framework'),
+                    'model': result['metric'].get('container_label_model', 'unknown_model'),
+                    'batch_size': result['metric'].get('container_label_batch_size', 'unknown_batch_size'),
+                    'learning_rate': result['metric'].get('container_label_learning_rate', 'unknown_learning_rate'),
+                    'num_epochs': result['metric'].get('container_label_num_epochs', 'unknown_num_epochs'),
+                }
+
+                value = float(result['value'][1])
+                metrics_data += (f'{unix_time} container_cpu_utilization('
+                                 f'image:{labels["image"]},container:{labels["name"]},'
+                                 f'dataset:{labels["dataset"]},framework:{labels["framework"]},'
+                                 f'model:{labels["model"]},batch_size:{labels["batch_size"]},'
+                                 f'learning_rate:{labels["learning_rate"]},num_epochs:{labels["num_epochs"]}) '
+                                 f'{value:.2f}%\n')
 
 @app.route('/metrics', methods=['GET'])
 def get_metrics():
@@ -78,4 +84,5 @@ if __name__ == "__main__":
 
     # Start the Flask application
     app.run(host='0.0.0.0', port=4223)
+
 
